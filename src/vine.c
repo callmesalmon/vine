cadb198091e9282f34995980be9927f424b9d8ae
Elis Staaf <elis.staaf@proton.me>
2025-01-05 13:27:45 +0100

    c)24

--- src/vine.c
+++ src/vine.c
@@ -1,5 +1,5 @@
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
- * Copyright (c) 2025 Elis Staaf                                             *
+ * Copyright (c) 2024 Elis Staaf                                             *
  *                                                                           *
  * Licensed under the Apache License, Version 2.0 (the "License");           *
  * you may not use this file except in compliance with the License.          *
