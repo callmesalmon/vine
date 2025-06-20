ff71f004dad95d1d08d246e723ca61f12d31ea28
salmon <elis.staaf@proton.me>
2025-06-20 22:21:13 +0200

    fix: Switch "ownership" (not really)

--- src/vine.c
+++ src/vine.c
@@ -1,5 +1,5 @@
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
- * Copyright (c) 2024 Elis Staaf                                             *
+ * Copyright (c) 2024 Salmon                                                 *
  *                                                                           *
  * Licensed under the Apache License, Version 2.0 (the "License");           *
  * you may not use this file except in compliance with the License.          *
