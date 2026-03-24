c1f0d947804ab3ffaa8dd7b89409705495c4deb9
salmon <staaf.elis@gmail.com>
2026-03-24 16:10:47 +0100

    "correct" license
    i was a bit concerned about the legal implications of relicensing...

--- vine.c
+++ vine.c
@@ -1,20 +1,4 @@
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
- * Copyright (c) 2024 Salmon                                                 *
- *                                                                           *
- * Licensed under the Apache License, Version 2.0 (the "License");           *
- * you may not use this file except in compliance with the License.          *
- * You may obtain a copy of the License at                                   *
- *                                                                           *
- *     http://www.apache.org/licenses/LICENSE-2.0                            *
- *                                                                           *
- * Unless required by applicable law or agreed to in writing, software       *
- * distributed under the License is distributed on an "AS IS" BASIS,         *
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  *
- * See the License for the specific language governing permissions and       *
- * limitations under the License.                                            *
- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
-
-/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
  * VINE EDITOR                                                               *
  *                                                                           *
  * VINE is an extension of Kilo (think of the VI/VIM dynamic) with many      *
@@ -26,6 +10,8 @@
  * This is the VINE source code, feel free to contribute on                  *
  * <https://github.com/callmesalmon/vine>. Or you can just enjoy             *
  * the editor, I won't judge! And with that, enjoy!                          *
+ *                                                                           *
+ * LICENSE: BSD-2-Clause                                                     *
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
 #define _DEFAULT_SOURCE
