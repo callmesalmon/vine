b952dbda026a0e32414d6be395f801392fb8989b
salmon <staaf.elis@gmail.com>
2026-03-25 22:44:20 +0100

    fix a teeny segfault because of referencing unused memory

--- vine.c
+++ vine.c
@@ -999,7 +999,7 @@ void editorDrawStatusBar(struct abuf *ab) {
     abAppend(ab, "\x1b[7m", 4);
     char status[80], rstatus[80];
 
-    if (strlen(E.filename) > 32) {
+    if (E.filename && strlen(E.filename) > 32) {
         for (int i = 29; i < 32; i++)
             E.filename[i] = '.';
     }
