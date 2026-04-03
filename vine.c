a7c876561e5fc0f2baa260af66fc4d940b377efd
salmon <staaf.elis@gmail.com>
2026-04-03 22:43:50 +0200

    make numbers in default theme red

--- vine.c
+++ vine.c
@@ -207,7 +207,7 @@ const struct editorTheme elflord_theme = {
 
 const struct editorTheme default_theme = {
     BRIGHT_BLUE, BRIGHT_CYAN, GREEN, BRIGHT_YELLOW,
-    BRIGHT_PURPLE, BLUE, WHITE,
+    BRIGHT_RED, BLUE, WHITE,
 
     GREY, WHITE, BG_DEFAULT
 };
